adfadf456a0cde7fff2e0b3802c3b851fd0a2922
Jannis Klinkenberg <jannis.klinkenberg@rwth-aachen.de>
2021-10-27 13:04:38 +0200

    added option to have last private for read only

--- experiments/STREAM/run_stream_numa.sh
+++ experiments/STREAM/run_stream_numa.sh
@@ -5,7 +5,9 @@ echo "===         Bandwidth Tests          ==="
 echo "========================================"
 
 # N_THREADS_PER_DOMAIN=(1 2 4 6 8 10 12 14 16 18 20 22 24)
-N_THREADS_PER_DOMAIN=(1 2 4 6 8 10 12 14 16 18 20)
+# N_THREADS_PER_DOMAIN=(1 2 4 6 8 10 12 14 16 18 20)
+N_THREADS_PER_DOMAIN=(1 2 3 4 5 6 7 8 9 10 11 12)
+# N_THREADS_PER_DOMAIN=(8)
 N_REP=3
 
 # display hardware overview
@@ -17,7 +19,7 @@ RES_SEP="\t"
 # build benchmark once
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 BENCH_DIR="${SCRIPT_DIR}/../../benchmarks/STREAM"
-READ_ONLY=0 NTIMES=10 STREAM_ARRAY_SIZE=200000000 make stream.icc --directory=${BENCH_DIR}
+READ_ONLY=0 READ_ONLY_REDUCTION=1 NTIMES=10 STREAM_ARRAY_SIZE=200000000 make stream.icc --directory=${BENCH_DIR}
 
 # get all domains containing CPUs
 CPU_DOMAINS="$(numactl -H | grep cpus | awk '(NF>3) {printf "%d ", $2}' | sed 's/.$//')"
